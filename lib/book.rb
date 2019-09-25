1caa996c8afd67cf39ed5315554764fe0eba66eb
Charity Parks <tuppercharity@gmail.com>
2019-09-25 21:11:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,9 +4,29 @@ class Book
   end
   
   def title 
-    
+    @title
+  end
+  
+  def initialize(author)
     @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def initialize(page_count)
     @page_count = page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def initialize(genre)
     @genre = genre
   end
+  
+  def genre
+    
 end
